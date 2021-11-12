248af8fd3e69ce196779fdbca7a56c620d5d580f
joelcassel <joel@familjencassel.se>
2021-11-12 10:26:06 +0100

    Initial config

---
+++ src/easee.js
@@ -0,0 +1,74 @@
+const axios = require('axios').default
+
+// API Details for Easee : https://developer.easee.cloud/docs/get-started
+
+let accessToken = null
+const onlyOneChargerIdDefault = 'EH2KYUBH'
+const apiUrl = 'https://api.easee.cloud'
+const username = process.env.EASEE_USERNAME || 'username'
+const password = process.env.EASEE_PASSWORD || 'password'
+
+async function initAccessToken() {
+  if (accessToken) {
+    console.log('Reusing access token..')
+    resolve(accessToken)
+  }
+  console.log('Query new access token..')
+  const response = await axios
+    .post(apiUrl + '/api/accounts/token', {
+      userName: username,
+      password: password,
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+  accessToken = response.data.accessToken
+  //Set global token
+  console.log('Token retrieved..')
+  axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+  return accessToken
+}
+
+async function easeeGetCall(endpoint) {
+  console.log(`Calling endpoint ${endpoint} with token...`)
+  const { data } = await axios.get(apiUrl + endpoint).catch(function (error) {
+    console.log(error)
+  })
+  //console.log(data);
+  return data
+}
+
+async function getChargers() {
+  const response = await easeeGetCall('/api/chargers')
+  return response
+}
+
+async function getChargerDetails(chargerId = onlyOneChargerIdDefault) {
+  const response = await easeeGetCall(`/api/chargers/${chargerId}/details`)
+  return response
+}
+
+async function getWeeklySchedule(chargerId = onlyOneChargerIdDefault) {
+  const response = await easeeGetCall(
+    `/api/chargers/${chargerId}/weekly_charge_plan`,
+  )
+  return response
+}
+
+async function start() {
+  console.log('Started!')
+  await initAccessToken()
+
+  const chargers = await getChargers()
+  chargers.forEach((charger) => console.log(`--- Charger: ${charger.name} (${charger.id})`))
+  
+  const chargerDetails = await getChargerDetails(chargers[0].id)
+  console.log(chargerDetails);
+  
+  /*
+  const schedule = await getWeeklySchedule()
+  console.log(JSON.stringify(schedule, null, 2));
+  */
+}
+
+start()
