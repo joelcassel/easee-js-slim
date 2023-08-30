05906ef13f47bd0702a47d82ed5ffbb2d8e1cb72
joelcassel <joel@familjencassel.se>
2023-08-30 22:01:50 +0200

    Updated formatting

--- src/integration/chargerOpMode.js
+++ src/integration/chargerOpMode.js
@@ -1,12 +1,12 @@
 // Could not find this on the official docs, copied from https://www.npmjs.com/package/iobroker.easee
- 
+
 const chargerOpMode = {
   Offline: 0,
-  Disconnected: 1, 
-  AwaitingStart: 2, 
-  Charging: 3, 
-  Completed: 4, 
-  Error: 5, 
-  ReadyToCharge: 6
+  Disconnected: 1,
+  AwaitingStart: 2,
+  Charging: 3,
+  Completed: 4,
+  Error: 5,
+  ReadyToCharge: 6,
 }
 export default chargerOpMode
